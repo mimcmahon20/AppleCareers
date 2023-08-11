43c6affc25d2368fdaabc57cebb17d044c876bb4
mimcmahon20 <mimcmahon20@vt.edu>
2023-08-11 15:34:03 -0400

    semi-functional first 3 sections, running into height issues

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -3,6 +3,7 @@ import "../styles/Homepage.css";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import photoonedesktop from "../assets/photoonedesktop.jpg";
+import phototwodesktop from "../assets/phototwodesktop.jpg";
 
 gsap.registerPlugin(ScrollTrigger);
 
@@ -15,6 +16,10 @@ export default function Homepage() {
   const photoOneRef = useRef(null);
   const firstTextUnderPhotoRef = useRef(null);
   const firstTextUnderPhotoContainerRef = useRef(null);
+
+  const photoTwoRef = useRef(null);
+    const secondTextUnderPhotoRef = useRef(null);
+    const secondTextUnderPhotoContainerRef = useRef(null);
 
   useEffect(() => {
     let tlVideoText = gsap.timeline({});
@@ -81,36 +86,90 @@ export default function Homepage() {
           start: "top center",
           end: "center top",
           scrub: true,
-          markers: true,
+          markers: false,
         },
       }
     );
+
+    let tlPhotoTwoText = gsap.timeline({});
+    tlPhotoTwoText.fromTo(
+        photoTwoRef.current.children[0],
+        { y: 500 },
+        {
+            y: 0,
+            duration: 1,
+            scrollTrigger: {
+                trigger: photoTwoRef.current,
+                start: "center center-=250",
+                end: "bottom center",
+                scrub: true,
+                markers: true,
+            },
+        }
+    );
+
+    let tlTextUnderPhotoTwo = gsap.timeline({});
+    tlTextUnderPhotoTwo.fromTo(
+        secondTextUnderPhotoRef.current,
+        { opacity: 0 },
+        {
+            opacity: 1,
+            duration: 1,
+            scrollTrigger: {
+                trigger: secondTextUnderPhotoContainerRef.current,
+                start: "center center",
+                end: "top top",
+                scrub: true,
+                markers: false,
+            },
+        }
+    );
+
+
 
     window.addEventListener("scroll", () => {
       if (
         firstTextUnderPhotoContainerRef.current.getBoundingClientRect().y <= 0
       ) {
-        console.log("hello");
         firstTextUnderPhotoRef.current.style.position = "unset";
+        photoTwoRef.current.style.display = "block";
       } else {
         firstTextUnderPhotoRef.current.style.position = "fixed";
+        photoTwoRef.current.style.display = "none";
+        secondTextUnderPhotoRef.current.style.display = "none";
       }
+
+      if(secondTextUnderPhotoContainerRef.current.getBoundingClientRect().y <= 0) {
+        secondTextUnderPhotoRef.current.style.display = "block";
+        secondTextUnderPhotoRef.current.style.position = "unset";
+      } else { 
+        secondTextUnderPhotoRef.current.style.position = "fixed";
+
+      }
+
+      
     });
   }, []);
+
+  
 
   return (
     <div className="homepage">
       <div className="hero-video-relative-container" ref={videoWrapperRef}>
         <section className="hero-video-wrapper" ref={videoContainerRef}>
-          <div className="hero-video"></div>
+          <div className="hero-video">
+            <video className="hero-video" autoPlay muted loop>
+                <source src="/Videos/herovideo.mp4" type="video/mp4" />
+            </video>
+          </div>
         </section>
         <section className="hero-text-container" ref={initialVideoTextRef}>
           <h2 className="hero-text">Students</h2>
           <h1 className="hero-text">Gain more than experience.</h1>
           <h1 className="hero-text">Gain an Apple experience.</h1>
         </section>
-        <section className="hero-text-container" ref={finalVideoTextRef}>
-          <h1 className="hero-text">Hello!</h1>
+        <section className="hero-text-container-secondary" ref={finalVideoTextRef}>
+          <h1 className="hero-text">Set your sights high. Because experience is about more than work itself - it's about how you learn and who you learn from. At Apple, you`ll start a career doing important work with people who`ve made Apple what it is today. So that you can make it what it is tomorrow.</h1>
         </section>
       </div>
 
@@ -193,6 +252,55 @@ export default function Homepage() {
           </div>
         </section>
       </div>
+
+
+      <section className="section-fullscreen">
+        <div className="photo-section-container" ref={photoTwoRef}>
+          <section className="photo-text right">
+            <h1 className="photo-quote">
+            “You`re really seen as the Apple leader for your community — while you`re contributing to campus life.”
+
+            </h1>
+            <h3 className="photo-reference">
+            Jacob, Apple Sales account executive and former Apple Campus Leader
+
+
+            </h3>
+            <button className="photo-link">Read Jacob's Story</button>
+          </section>
+          <img
+            src={phototwodesktop}
+            alt="photoonedesktop"
+            className="photo-section-image"
+          />
+        </div>
+      </section>
+
+      <div className="text-location" ref={secondTextUnderPhotoContainerRef}>
+        <section
+          className="section-fullscreen absolute-under clear"
+          ref={secondTextUnderPhotoRef}
+        >
+          <div className="text-section-container fixed">
+            <h1 className="large-text padding-top">Apple Support College Advisor
+</h1>
+            <p className="small-text padding-bottom">
+            At participating universities, you can join us part-time, working on the team that provides world-class technical support. When customers contact Apple for help, Advisors are their human connection: friendly, thoughtful, and real. You`ll answer questions about our products and services, and you`ll troubleshoot issues. There`s no need to be a tech expert. We`ll train you, so you`re ready to help people get back to doing what they love. A big part of your training covers the professional and collaborative communication skills that are so important for this role and any career. It`s the listening skills you`ll develop that make it clear you`re not simply supporting technology — you`re supporting people.
+
+
+            </p>
+            <div className="text-links">
+              <button className="link">
+                Find Available Apple Support College Advisor opportunities
+              </button>
+              <button className="link">
+                Learn more about the Apple Support College Advisor role
+              </button>
+            </div>
+          </div>
+        </section>
+      </div>
+
     </div>
   );
 }
